7edb96104089e046961b8e7b1199d047dd30931d
jhebert <jhebert@9984d3b4-7e2a-0410-a31b-dfae3d5ab6e1>
2007-03-01 00:56:21 +0000

    Better.

--- trunk/tracer.py
+++ trunk/tracer.py
@@ -19,6 +19,7 @@ class Tracer:
         data=open('./traces/'+name).read().split('\n')
         f=open('test-trace-'+name,'w')
         oldTime,total,count=0,len(data),0
+        oldLat, oldLon = 0.0, 0.0
         for line in data:
             count+=1
             print str(count)+'/'+str(total)
@@ -29,8 +30,14 @@ class Tracer:
             self.locator.Update(mac, 10**((-32-int(ss))/25.0))
             if(t>oldTime):
                 if(oldTime!=0):
+                    lat,lon=self.locator.ReturnOldBestParticle()
+                    f.write(str(lon)+'\t'+str(lat))
                     lat,lon=self.locator.GetLocation()
-                    f.write(str(lon)+'\t'+str(lat)+'\n')
+                    f.write('\t'+str(lon)+'\t'+str(lat))
+                    if(oldLat!=0.0):
+                        f.write('\t'+str((lon+oldLon)/2)+'\t'+str((lat+oldLat)/2))
+                    oldLat,oldLon=lat,lon
+                    f.write('\n')
                     f.flush()
                 self.locator.ReSample()
                 oldTime=t
